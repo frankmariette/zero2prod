6237f6d5c2ffc4a87c065539e3d55829b556c9d7
Francois Mariette <karrde00@gmail.com>
2023-03-06 20:43:37 -0800

    ch3.5 complete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed ot bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Server running at 127.0.0.1:{}", port);
+    run(listener)?.await
 }
