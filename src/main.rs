def9718142aef5d4a14657223c66e88f9d562d42
Francois Mariette <karrde00@gmail.com>
2023-03-14 23:34:34 -0700

    adds tests with postgres

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed ot bind random port");
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     println!("Server running at 127.0.0.1:{}", port);
     run(listener)?.await
 }
+
+
