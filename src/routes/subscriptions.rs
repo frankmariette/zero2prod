3ce210d2877590d668ed81f77941f5ab6806ab75
Francois Mariette <karrde00@gmail.com>
2023-04-23 12:54:57 -0700

    chapter 4 complete. tracing added to application, secrets not exposed in logs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,6 @@
 use actix_web::{web, HttpResponse};
 use sqlx::types::chrono::Utc;
 use sqlx::PgPool;
-use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -10,21 +9,13 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "adding a new subscriber",
-        %request_id,
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
-    let _request_span_guard = request_span.enter();
-    let query_span = tracing::info_span!(
-        "Saving new subscriber details in DB"
-    );
-
-    match sqlx::query!(
-        r#"
+#[tracing::instrument(
+    name = "Saving new subscriber details in database",
+    skip(form,pool)
+)]
+pub async fn insert_subscriber(form: &FormData, pool: &PgPool) -> Result<(), sqlx::Error> {
+   sqlx::query!(
+       r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
@@ -32,18 +23,28 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         form.email,
         form.name,
         Utc::now()
+   )
+       .execute(pool)
+       .await
+       .map_err(|e| {
+           tracing::error!("Failed to execute query: {:?}", e);
+           e
+       })?;
+    Ok(())
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields(
+        subscriber_email = %form.email,
+        subsriber_name = %form.name
     )
-    .execute(pool.get_ref())
-    .instrument(query_span)
-    .await
-    {
-        Ok(_) => {
-            tracing::info!("request_id: {} - New subscriber details in the DB", request_id);
-            HttpResponse::Ok().finish()
-        },
-        Err(e) => {
-            tracing::info!("request_id: {} - Failed to execute query: {:?}", request_id, e);
-            HttpResponse::InternalServerError().finish()
-        }
+)]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+
+    match insert_subscriber(&form, &pool).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish()
     }
 }
